96c68ceb5598a36eeb07d86ded0a2ba6a2cebfa8
SaimahadevG <saimahadevg@gmail.com>
2025-03-02 15:25:24 +0530

    initial sturucture

---
+++ src/main/java/com/ImprovedStudentCrud/ImprovedStudentCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.ImprovedStudentCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovedStudentCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovedStudentCrudOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ImprovedStudentCrud/ImprovedStudentCrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ImprovedStudentCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovedStudentCrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
